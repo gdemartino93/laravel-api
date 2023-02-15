479ed1889fbae75e5feed209872ca818c9d929e3
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-15 19:28:10 +0100

    scaffolding frontend

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<div class="text-center text-info col-12 d-flex align-items-center">
+    <div class="col-2">
+       <a href="">
+          <i class="fa-solid fa-house fs-2"></i>
+       </a>
+       <a href="" class="mx-2">
+          <i class="fa-solid fa-plus fs-2"></i>
+       </a>
+    </div>
+    <div class="col-8">
+        <a href="" class="text-center">
+          <h1>My minimal Shop</h1>
+        </a>
+       </div>
+    <div class="col-2">
+       <a href="" class="fs-2">All Products</a>
+    </div>
+  
+ </div>
