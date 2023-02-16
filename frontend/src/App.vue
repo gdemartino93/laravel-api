b5d420d34a8935339faf2afc6ab9a820250629fe
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-16 19:33:49 +0100

    create header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,28 @@
 <script >
+import AppHeader from './components/AppHeader.vue'
+
+export default{
+  components :{ AppHeader} ,
+  data(){
+    return{
+      title : "My Minimal Shop"
+    }
+  }
+}
 </script>
 
 <template>
-  
+  <header>
+    <AppHeader :title="title" />
+  </header>
+  <main>
+    
+  </main>
+
 </template>
 
-<style scoped>
+<style >
+
+
 
 </style>
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<script >
+export default {
+    props: {
+        title : String
+    },
+    data(){
+        return{
+            
+        }
+    },
+  components: {
+
+  }
+}
+</script>
+<template>
+ <div class="text-center text-info col-12 d-flex align-items-center">
+    <div class="col-2">
+        <a href="http://127.0.0.1:8000/movies/create">
+            <i class="fa-solid fa-plus fs-2"></i>
+        </a>
+        <a href="http://localhost:5174/">
+            <i class="fa-solid fa-house fs-2"></i>
+        </a>
+    </div>
+    <div class="col-8">
+        <a href="http://localhost:5174/">
+            <h1>My minimal Shop</h1>
+        </a>
+    </div>
+    <div class="col-2">
+       All Movies
+    </div>
+  
+ </div>
+</template>
+
+
+<style scoped>
+a{
+    text-decoration: none;
+}
+a:hover{
+    color: inherit;
+}
+</style>
