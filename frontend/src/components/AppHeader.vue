7be4b1cf42d59b02ee6f36313fa80fd0ba291b4d
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-21 19:54:54 +0100

    createed form and button to show it

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -16,15 +16,13 @@ export default {
 <template>
  <div class="text-center text-info col-12 d-flex align-items-center">
     <div class="col-2">
-        <a href="http://127.0.0.1:8000/movies/create">
-            <i class="fa-solid fa-plus fs-2"></i>
-        </a>
-        <a href="http://localhost:5174/">
+
+        <a href="">
             <i class="fa-solid fa-house fs-2"></i>
         </a>
     </div>
     <div class="col-8">
-        <a href="http://localhost:5174/">
+        <a href="">
             <h1>{{title}}</h1>
         </a>
     </div>
--- frontend/src/components/AppMain.vue
+++ frontend/src/components/AppMain.vue
@@ -2,12 +2,20 @@
 import axios from 'axios';
 
 axios.defaults.baseURL ="http://127.0.0.1:8000/api/v1/movie/"
+
+const emptyNewMovie = {
+  tags_id : []
+};
 export default{
   data(){
     return{
       movies : [],
       tags : [],
-      genres : []
+      genres : [],
+      // nuovo oggetto creato dal form
+      new_movie : emptyNewMovie,
+      // gestione form visibile
+      isVisible : false
     }
   },
   methods : {
@@ -38,6 +46,14 @@ export default{
         
           })
           .catch(err => console.log(err))
+    },
+    showForm(e){
+      e.preventDefault()
+      this.isVisible = !this.isVisible
+    },  
+    submitMovie(e){
+      e.preventDefault();
+       
     }
   },
   mounted(){
@@ -48,18 +64,33 @@ export default{
 </script>
 
 <template>
-  <form action="" method="POST" class="d-flex flex-column col-4 mx-auto">
+        <a href="" @click="showForm">
+            <i class="fa-solid fa-plus fs-2"></i>
+        </a>
+      <form v-if="isVisible === true" action="" method="POST" class="d-flex flex-column col-4 mx-auto">
     <label for="name">Name</label>
-    <input type="text" name="name" id="" v-model="name">
+    <input type="text" name="name" id="" v-model="new_movie.name">
     <label for="year">Year</label>
-    <input type="number" name="year" id="">
+    <input type="number" id="" v-model="new_movie.year">
     <label for="cashout">CashOut</label>
-    <input type="number" name="cashout" id="">
+    <input type="number" name="cashout" id="" v-model="new_movie.cashout">
     <br>
-    <select name="genre_id" class="my-3">
-      <option value="" v-for="genre in genres" id="genres">{{ genre.name }}</option>
+    <label for="genre_id">Genre:</label>
+    <br>
+    <select name="genre_id" class="my-3" v-model="new_movie.genre">
+      <option v-for="genre in genres" :value="genre.id" key="genre.id"  >{{ genre.name }}</option>
     </select>
+    <span>TAG:</span>
+      <br>
+    <div v-for="tag in tags" key="tag.id">
+      <input type="checkbox" name="" :id="tag.id" :value="tag.id" v-model="new_movie.tags_id">
+      <label for="{{ tag.id }}">{{ tag.name }}</label>
+    </div> 
+    <div>
+      <button class="btn btn-success my-3" type="submit">Add</button>
+    </div>
   </form>
+  
   <h1 class="text-success">Movies:</h1>
   <div v-for="movie in movies" class="d-flex flex-column col-6 myItem" :key="movie.id">
     <h4 >Name: {{ movie.name }}</h4>
@@ -68,9 +99,7 @@ export default{
     <span @click="deleteMovie(movie)">
       <i class="fa-solid fa-trash"></i>
     </span>
-    <div>
-      <button class="btn btn-success">Add</button>
-    </div>
+
   </div>
   
 </template>
