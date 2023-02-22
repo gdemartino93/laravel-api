0b2f754fc221e6ea8d278bae520f7fed46070072
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-22 10:08:21 +0100

    error

--- frontend/src/components/AppMain.vue
+++ frontend/src/components/AppMain.vue
@@ -57,9 +57,14 @@ export default{
     },  
     editMovie(movie){
 ;
-      this.new_movie = movie
-      console.log(this.new_movie);
-      // axios.post('edit/' + movie)
+      this.new_movie = {...movie};
+      this.isVisible = true;
+      this.new_movie.tags_id = [];
+      console.log("movie.tag" + this.new_movie);
+      for (let x = 0 ; x < movie.tags.length; x++){
+        const tag = movie.tags[x];
+        this.new_movie.tags_id.push(tag.id)
+      }
     },
     deleteMovie(movie){
       axios.delete('delete/' + movie.id)
@@ -94,30 +99,30 @@ export default{
             <i class="fa-solid fa-plus fs-2"></i>
         </a>
       <form v-if="isVisible === true" action=""  class="d-flex flex-column col-4 mx-auto">
-    <label for="name">Name</label>
-    <input type="text" name="name"  v-model="new_movie.name">
-    <label for="date">Year</label>
-    <input type="number" name="date" v-model="new_movie.date">
-    <label for="cashOut">CashOut</label>
-    <input type="number" name="cashOut"  v-model="new_movie.cashOut">
-    <br>
-    <label for="genre_id">Genre:</label>
-    <br>
-    <select name="genre_id" class="my-3" v-model="new_movie.genre_id">
-      <option v-for="genre in genres" :value="genre.id" :key="genre.id"  >{{ genre.name }}</option>
-    </select>
-    <span>TAG:</span>
-      <br>
-    <div v-for="tag in tags" :key="tag.id">
-      <input type="checkbox" name="" :id="tag.id" :value="tag.id" v-model="new_movie.tags_id">
-      <label :for="tag.id">{{ tag.name }}</label>
-    </div> 
-    <div>
-      <button class="btn btn-success my-3" type="submit" @click="storeMovie">Add</button>
-      <button class="btn btn-danger" @click="closeForm">Close Form</button>
-    </div>
-  </form>
-  
+          <label for="name">Name</label>
+          <input type="text" name="name"  v-model="new_movie.name">
+          <label for="date">Year</label>
+          <input type="number" name="date" v-model="new_movie.date">
+          <label for="cashOut">CashOut</label>
+          <input type="number" name="cashOut"  v-model="new_movie.cashOut">
+          <br>
+          <label for="genre_id">Genre:</label>
+          <br>
+          <select name="genre_id" class="my-3" v-model="new_movie.genre_id">
+            <option v-for="genre in genres" :value="genre.id" :key="genre.id"  >{{ genre.name }}</option>
+          </select>
+          <span>TAG:</span>
+            <br>
+          <div v-for="tag in tags" :key="tag.id">
+            <input type="checkbox" name="tag" :id="tag.id" :value="tag.id" v-model="new_movie.tags_id">
+            <label :for="tag.id">{{ tag.name }}</label>
+          </div> 
+          <div>
+            <button class="btn btn-success my-3" type="submit" @click="storeMovie">Add</button>
+            <button class="btn btn-danger" @click="closeForm">Close Form</button>
+          </div>
+      </form>
+      
   <h1 class="text-success">Movies:</h1>
   <div v-for="movie in movies" class="d-flex flex-column col-6 myItem" :key="movie.id">
     <h4 >Name: {{ movie.name }}</h4>
